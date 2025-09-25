bf720651368d5b2d3ce9f86270d8855aad29010f
kev1n999 <sopratesta999@gmail.com>
2025-09-25 18:27:53 -0300

    feat: add navbar + logo

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,18 +1,44 @@
+import logo from "../../assets/logo.png";
+
 function NavBar() {
   return (
-    <nav className="flex justify-center h-3.5">
-      <ul className="flex gap-3.5 justify-between text-white bg-amber-200">
+    <nav className="flex justify-center items-center w-full h-20 bg-neutral-800">
+      <div className="w-full max-w-2xl flex flex-row items-center">
+        <img src={logo} className="h-10" alt="" />
+        <h1 className="text-white font-extrabold ">TechLab</h1>
+      </div>
+      <ul className="flex gap-3.5 justify-between text-white">
         <li>
-          <a href="#home">Home</a>
+          <a
+            className="relative pb-1.5 border-b-2 border-transparent transition-all duration-400 hover:border-white"
+            href="#home"
+          >
+            Home
+          </a>
         </li>
         <li>
-          <a href="#products">Products</a>
+          <a
+            className="relative pb-1.5 border-b-2 border-transparent transition-all duration-400 hover:border-white"
+            href="#products"
+          >
+            Products
+          </a>
         </li>
         <li>
-          <a href="#contacts">Contacts</a>
+          <a
+            className="relative pb-1.5 border-b-2 border-transparent transition-all duration-400 hover:border-white"
+            href="#contacts"
+          >
+            Contacts
+          </a>
         </li>
         <li>
-          <a href="#discord">Discord</a>
+          <a
+            className="relative pb-1.5 border-b-2 border-transparent transition-all duration-400 hover:border-white"
+            href="#discord"
+          >
+            Discord
+          </a>
         </li>
       </ul>
     </nav>
